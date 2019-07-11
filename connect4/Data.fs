71c43959f0e47eea3f1adbc7415fdd497085a9ea
Thibault <cam@intechinfo.fr>
2019-07-11 11:02:50 +0200

    Start model

---
+++ connect4/Data.fs
@@ -0,0 +1,31 @@
+﻿namespace connect4.Data
+
+open System.Collections.Generic
+
+type Pawn = 
+| Red
+| Yellow
+| Void
+
+type ListPawn =
+| Empty
+| Item of Pawn * ListPawn
+
+type State = {
+  Map: ListPawn
+}
+
+module Data =
+
+  let mutable private stateStorage = ListPawn(Empty)
+  let sendResult () =
+    stateStorage |> Seq.map (fun p -> p)
+
+  let getState state =
+    let newState = {
+        Map = state.Map
+    }
+    stateStorage <- newState.Map
+    newState
+
+
--- connect4/Program.fs
+++ connect4/Program.fs
@@ -1,8 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+open connect4.Rest
+open connect4.Data
 open System
+open Suave
+open Suave.Successful
+
+//[<EntryPoint>]
+//let main argv =
+//  startWebServer defaultConfig (OK "Hello, Suave!")
+//  0
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+  let personWebPart = rest "people" {
+    GetAll = Data.sendResult
+    Create = Data.getState
+  }
+  startWebServer defaultConfig personWebPart
+  0
+
+
+
+
