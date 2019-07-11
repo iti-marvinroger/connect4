11bd0d8dd60b27dfe21dbfbe497ca730ee05105c
ThibaultCam <cam@intechinfo.fr>
2019-07-11 17:28:49 +0200

    feat: add storage and REST implementation (#1)
    * Start model
    
    * modif
    
    * D'equere model
    
    * fix

---
+++ connect4/Data.fs
@@ -0,0 +1,31 @@
+﻿namespace connect4.Data
+
+type Pawn = 
+| Red
+| Yellow
+
+type BoardCell = Option<Pawn>
+type BoardColumn = array<BoardCell>
+type Board = array<BoardColumn>
+
+type Pos = {
+ X : int
+ Y : int
+}
+
+
+type State = {
+  LastPawn: Pos
+  Map: Board
+}
+
+module Data =
+  let mutable private stateStorage = { LastPawn = { X = 0; Y = 0 }; Map = (Array.create 7 (Array.create 6 None))}
+  let sendResult () = stateStorage
+  let getState state =
+    let newState = {
+        LastPawn = state.LastPawn
+        Map = state.Map
+    }
+    stateStorage <- newState
+    newState
