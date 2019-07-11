988ae349520d4915c3557ee8512db12f69df4631
Thibault <cam@intechinfo.fr>
2019-07-11 15:53:27 +0200

    D'equere model

--- connect4/Data.fs
+++ connect4/Data.fs
@@ -3,26 +3,29 @@
 type Pawn = 
 | Red
 | Yellow
-| Void
 
-type ListPawn =
-| Empty
-| Item of Pawn * ListPawn
+type BoardCell = Option<Pawn>
+type BoardColumn = array<BoardCell>
+type Board = array<BoardColumn>
+
+type Pos = {
+ X : int
+ Y : int
+}
+
 
 type State = {
-  CurrentPos: ListPawn
-  Map: ListPawn
+  LastPawn: Pos
+  Map: Board
 }
 
 module Data =
-  let mutable private stateStorage = Item(Red, Empty)
+  let mutable private stateStorage = { LastPawn = { X = 0; Y = 0 }; Map = (Array.create 7 (Array.create 6 None))}
   let sendResult () = stateStorage
   let getState state =
     let newState = {
-        CurrentPos = state.CurrentPos
+        LastPawn = state.LastPawn
         Map = state.Map
     }
-    stateStorage <- newState.Map
+    stateStorage <- newState
     newState
-
-
